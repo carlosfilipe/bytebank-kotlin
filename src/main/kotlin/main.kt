21342f3cc46ac87d73ccc67f2e282e8d789941a8
Carlos Filipe <carlosfilipe.lb@gmail.com>
2021-05-18 18:45:32 -0300

    if and when

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,7 +1,25 @@
 fun main(){
 
-    val titular = "Carlos"
+    val titular: String = "Carlos"
+    val numeroConta: Int = 1000
+    var saldo: Double = 0.0
+
+//    saldo = 100.0
+//
+//    saldo += + 200
+    saldo = -1.0
+
     println("Titular: $titular")
+    println("Número da conta: $numeroConta")
+    println("Saldo: $saldo")
+
+    when {
+        saldo > 0.0 -> println("Conta positiva")
+        saldo == 0.0 -> println("conta zerada")
+        else -> println("conta negativa")
+    }
+
+
 
 }
 
